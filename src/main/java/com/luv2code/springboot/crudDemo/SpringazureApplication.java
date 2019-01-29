9f091fb284f5ed88303a6a75b85baea55a5b87c9
vcathot <vcathot@198.105.244.130>
2019-01-28 23:08:47 -0600

    initial commit

---
+++ src/main/java/com/luv2code/springboot/crudDemo/SpringazureApplication.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.crudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringazureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringazureApplication.class, args);
+	}
+
+}
+
